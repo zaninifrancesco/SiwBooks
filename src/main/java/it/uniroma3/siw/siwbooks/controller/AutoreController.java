25321b82be62b8c93803309d771c71748df8592f
Francesco Zanini <zaninifrancesco03@gmail.com>
2025-06-28 19:35:28 +0200

    Refactor AutoreController to simplify request mappings for authors

--- src/main/java/it/uniroma3/siw/siwbooks/controller/AutoreController.java
+++ src/main/java/it/uniroma3/siw/siwbooks/controller/AutoreController.java
@@ -5,22 +5,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import it.uniroma3.siw.siwbooks.service.AutoreService;
 import it.uniroma3.siw.siwbooks.model.Autore;
 
 @Controller
+@RequestMapping("/autori")
 public class AutoreController {
 
     @Autowired
     private AutoreService autoreService;
 
-    @GetMapping("/autori")
+    @GetMapping("")
     public String getAutori(Model model) {
         model.addAttribute("autori", autoreService.findAll());
         return "autori"; // Nome della vista Thymeleaf (es. autori.html)
     }
 
-    @GetMapping("/autori/{id}")
+    @GetMapping("/{id}")
     public String getAutore(@PathVariable("id") Long id, Model model) {
         Autore autore = autoreService.findById(id);
         if (autore != null) {
